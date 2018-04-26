b2852abbea44bff3c02096f908d37897b1e66e28
David Langen <david.langen@klosebrothers.de>
2018-04-26 19:45:45 +0200

    added Login/Logout functionality

---
+++ src/main/java/com/david/Config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.david.Config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                    .antMatchers("/*").hasRole("USER")
+                .and()
+                    .formLogin().loginPage("/login").permitAll()
+                .and()
+                    .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login?logout");
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        UserDetails david = User.withUsername("david").password("{noop}password").roles("USER", "ADMIN").build();
+        auth.inMemoryAuthentication().withUser(david);
+    }
+}
