3b0965edc7ab5658f6152889d9e3e5fea3f3bb26
David Langen <david.langen@klosebrothers.de>
2018-05-22 10:59:18 +0200

    added dummy data for Address

--- src/main/resources/data/data.sql
+++ src/main/resources/data/data.sql
@@ -1,4 +1,5 @@
-INSERT INTO students(student_number, first_name, last_name, mail, phone, date_of_birth) VALUES ('pfba416b', 'David', 'Langen', 'david.langen@sexyboy.de', '01008054646', '1996-06-10')
+INSERT INTO address(house_number,street,zip,town) VALUES ('1-3','Fürstenallee','33102','Paderborn')
+
 
 INSERT INTO courses(name,lecturer,description) values ('System- und Netzwerkprogrammierung','Prof. Dr. Nüßer','Man schieße sich ins Knie!')
 INSERT INTO courses(name,lecturer,description) values ('Wirtschaftsrecht','Pelke','Jeder ist mal Verbrecher!')
@@ -10,3 +11,7 @@ INSERT INTO courses(name,lecturer,description) values ('Wirtschaftsrecht','Pelke
 INSERT INTO courses(name,lecturer,description) values ('Wirtschaftsrecht','Pelke','Jeder ist mal Verbrecher!')
 INSERT INTO courses(name,lecturer,description) values ('Wirtschaftsrecht','Pelke','Jeder ist mal Verbrecher!')
 INSERT INTO courses(name,lecturer,description) values ('Wirtschaftsrecht','Pelke','Jeder ist mal Verbrecher!')
+
+INSERT INTO students(student_number, first_name, last_name, mail, phone, date_of_birth,ADDRESS_ID,) VALUES ('pfba416b', 'David', 'Langen', 'david.langen@sexyboy.de', '01008054646', '1996-06-10',1)
+
+INSERT INTO student_course(student_id, course_id) VALUES (1,1)
