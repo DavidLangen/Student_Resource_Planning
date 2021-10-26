0d28f92b646b64b9c77e06068b14c7bc456cd653
David Langen <david.langen@valtech.com>
2021-10-26 19:27:38 +0200

    add docs

---
+++ doc/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"com.david"},{"l":"com.david.Component"},{"l":"com.david.Config"},{"l":"com.david.Controller"},{"l":"com.david.Entity"},{"l":"com.david.Exceptions"},{"l":"com.david.Global"},{"l":"com.david.Repository"},{"l":"com.david.Service"}]
